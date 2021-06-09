3ebdd1ac540ddeef603b759fd4d79ff713a25960
Jairo Junior <jairo.ajunior@mercadolivre.com>
2021-06-09 13:08:08 -0300

    melhorando a classe

--- obtener-diploma/obtener-diploma/src/main/java/com/digitalhouse/obtenerdiploma/exceptions/ApiExceptionControllerAdvice.java
+++ obtener-diploma/obtener-diploma/src/main/java/com/digitalhouse/obtenerdiploma/exceptions/ApiExceptionControllerAdvice.java
@@ -1,14 +1,12 @@
 package com.digitalhouse.obtenerdiploma.exceptions;
 
 import com.digitalhouse.obtenerdiploma.dto.ErrorResponseDTO;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class ApiExceptionControllerAdvice{
